a1f4e762aae384dcca88af53768aa09577294562
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-21 18:23:22 +0100

    part 5: BONUS completed.

--- seed.js
+++ seed.js
@@ -1,13 +1,25 @@
-const Restaurant = require("./models/index")
-const { seedRestaurant } = require("./seedData");
-const db = require("./db/connection")
+const { Restaurant, Menu, Item } = require("./models/index");
+const { seedRestaurant, seedMenu, seedItem } = require("./seedData");
+const db = require("./db/connection");
 
 const syncSeed = async () => {
-    await db.sync({force: true});
-    await Restaurant.bulkCreate(seedRestaurant)
-    // BONUS: Update with Item and Menu bulkCreate
+  await db.sync({ force: true });
+  await Restaurant.bulkCreate(seedRestaurant);
+  await Menu.bulkCreate(seedMenu);
+  await Item.bulkCreate(seedItem);
+  populate();
+};
 
+const populate = async () => {
+  const restaurant_one = await Restaurant.findByPk(1);
+  const menu_one = await Menu.findByPk(1);
+  const menu_two = await Menu.findByPk(2);
+  const item_one = await Item.findByPk(1);
+  const item_two = await Item.findByPk(2);
+  const item_three = await Item.findByPk(3);
+  await menu_one.setItems([item_one, item_two]);
+  await menu_two.setItems([item_two, item_three]);
+  await restaurant_one.setMenus(menu_one, menu_two);
+};
 
-}
-
-syncSeed()
+syncSeed();
--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index");
+const { Restaurant, Menu, Item } = require("../models/index");
 const db = require("../db/connection");
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/restaurants", async (req, res) => {
-  const restaurants = await Restaurant.findAll();
+  const restaurants = await Restaurant.findAll({
+    include: [
+      {
+        model: Menu,
+        include: [
+          {
+            model: Item,
+          },
+        ],
+      },
+    ],
+  });
   res.json(restaurants);
 });
 
 app.get("/restaurants/:id", async (req, res) => {
-  const restaurant = await Restaurant.findByPk(req.params.id);
+  const restaurant = await Restaurant.findByPk(req.params.id, {
+    include: [
+      {
+        model: Menu,
+        include: [
+          {
+            model: Item,
+          },
+        ],
+      },
+    ],
+  });
   res.json(restaurant);
 });
 
