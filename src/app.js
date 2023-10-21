1ec8bdaed9390bd561133813b7fa1b3614b305ac
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-21 14:14:58 +0100

    part 2 complete.

--- src/app.js
+++ src/app.js
@@ -3,9 +3,42 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll();
   res.json(restaurants);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  res.json(restaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const newRestaurant = await Restaurant.create({
+    name: req.body.name,
+    location: req.body.location,
+    cuisine: req.body.cuisine,
+  });
+  res.json(newRestaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  const updatedRestuarant = await restaurant.update({
+    name: req.body.name,
+    location: req.body.location,
+    cuisine: req.body.cuisine,
+  });
+  res.json(updatedRestuarant);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  const deletedRestuarant = await restaurant.destroy();
+  res.json(deletedRestuarant);
 });
 
 module.exports = app;
